b510ba9e77a47f498bb0773c3d7fe534482dd76b
jacob weber <hello@jacobweber.net>
2018-03-18 22:34:08 -0500

    basic components setup and started.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,7 @@
 // let's go!
-// alert('heyooooo');
+import React from 'react';
+// could also import just the { Component } from 'react'
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));
