d43f9048ae8d330fcd1453b20fad1ec32a0356b5
jacob weber <hello@jacobweber.net>
2018-03-18 23:06:55 -0500

    importing basic styling.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -3,5 +3,6 @@ import React from 'react';
 // could also import just the { Component } from 'react'
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import './css/style.css';
 
 render(<StorePicker />, document.querySelector('#main'));
