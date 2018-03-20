6c374d0f4cbfac99b0bff48d69ec4be7df56ef17
jacob weber <hello@jacobweber.net>
2018-03-19 22:53:47 -0500

    Basic components created for order, inventroy, header and main app.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 // could also import just the { Component } from 'react'
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
