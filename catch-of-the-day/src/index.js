463c99b841f0c1af6689d513c7d93cbe264fa373
jacob weber <hello@jacobweber.net>
2018-03-21 22:44:36 -0500

    started setting up basic routes using React Router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,8 +2,7 @@
 import React from 'react';
 // could also import just the { Component } from 'react'
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
