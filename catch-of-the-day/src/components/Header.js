eb08d96a00a9af4f596b3c16b465dace1681309a
jacob weber <hello@jacobweber.net>
2018-03-22 22:49:12 -0500

    Helpers function and included.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = props => (
   <header className="top">
     <h1>
       Catch
-          <span className="ofThe">
+        <span className="ofThe">
         <span className="of">of</span>
         <span className="the">the</span>
       </span>
