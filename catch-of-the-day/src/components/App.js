b0ac3433c834b86399403fdba064d8ddc4f20ec2
jacob weber <hello@jacobweber.net>
2018-03-28 07:55:27 -0500

    Updating state, loading state and handling state onclick

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -20,14 +22,40 @@ class App extends React.Component {
       fishes
     });
   };
+  loadSampleFish = () => {
+    // alert('hit');
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    // 1. take a copy of state
+    const order = {...this.state.order};
+    // 2. add to order or update number in order
+    order[key] = order[key] + 1 || 1;
+    // 3. call setstate to update the state object
+    this.setState({ order });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Yo" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFish={this.loadSampleFish}
+        />
       </div>
     );
   }
